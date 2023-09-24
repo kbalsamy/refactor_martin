5016ecebb9ab1fd1c3c6e7aecd4e64c1e7b2753f
kbalsamy <bkarthikonmail@gmail.com>
2023-09-24 22:30:50 +0100

    ugly code

---
+++ Customer.py
@@ -0,0 +1,45 @@
+class Customer:
+    def __init__(self, name):
+        self._name = name
+        self._rentals = []
+
+    def add_rental(self, arg):
+        self._rentals.append(arg)
+
+    def get_name(self):
+        return self._name
+
+
+    def statement(self):
+        total_amount = 0
+        frequent_renter_points = 0
+        result = "Rental Record for " + self.get_name() + "\n"
+
+        for rental in self._rentals:
+            this_amount = 0
+
+            # Determine the rental amount based on the movie's price code and rental duration
+            if rental.get_movie().get_price_code() == Movie.REGULAR:
+                this_amount += 2
+                if rental.get_days_rented() > 2:
+                    this_amount += (rental.get_days_rented() - 2) * 1.5
+            elif rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
+                this_amount += rental.get_days_rented() * 3
+            elif rental.get_movie().get_price_code() == Movie.CHILDRENS:
+                this_amount += 1.5
+                if rental.get_days_rented() > 3:
+                    this_amount += (rental.get_days_rented() - 3) * 1.5
+
+            # Add bonus for a two-day new release rental
+            if rental.get_movie().get_price_code() == Movie.NEW_RELEASE and rental.get_days_rented() > 1:
+                frequent_renter_points += 1
+
+            # Display figures for this rental
+            result += "\t" + rental.get_movie().get_title() + "\t" + str(this_amount) + "\n"
+            total_amount += this_amount
+
+        # Add footer lines
+        result += "Amount owed is " + str(total_amount) + "\n"
+        result += "You earned " + str(frequent_renter_points) + " frequent renter points"
+
+        return result
