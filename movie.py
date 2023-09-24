44819b9aae56e37552c0e77179217667d6880184
kbalsamy <bkarthikonmail@gmail.com>
2023-09-25 00:57:01 +0100

    replacing condtional logic with polymorphism

--- movie.py
+++ movie.py
@@ -15,3 +15,26 @@ class Movie:
 
     def get_title(self):
         return self._title
+    
+    def get_charge(self, days_rented):
+
+        this_amount = 0
+        # Determine the rental amount based on the movie's price code and rental duration
+        if self.get_price_code() == self.REGULAR:
+            this_amount += 2
+            if days_rented > 2:
+                this_amount += (days_rented - 2) * 1.5
+        elif self.get_price_code() == self.NEW_RELEASE:
+            this_amount += days_rented * 3
+        elif self.get_price_code() == self.CHILDRENS:
+            this_amount += 1.5
+            if days_rented > 3:
+                this_amount += (days_rented - 3) * 1.5
+        return this_amount
+    
+    def get_frequent_renter_points(self, days_rented):
+
+        if self.get_price_code() == self.NEW_RELEASE and days_rented > 1:
+            return 2
+        else:
+            return 1
--- rental.py
+++ rental.py
@@ -13,24 +13,8 @@ class Rental:
     
     
     def get_charge(self):
-
-        this_amount = 0
-        # Determine the rental amount based on the movie's price code and rental duration
-        if self.get_movie().get_price_code() == Movie.REGULAR:
-            this_amount += 2
-            if self.get_days_rented() > 2:
-                this_amount += (self.get_days_rented() - 2) * 1.5
-        elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-            this_amount += self.get_days_rented() * 3
-        elif self.get_movie().get_price_code() == Movie.CHILDRENS:
-            this_amount += 1.5
-            if self.get_days_rented() > 3:
-                this_amount += (self.get_days_rented() - 3) * 1.5
-        return this_amount
+        return self._movie.get_charge(self.get_days_rented())
     
     def get_frequent_renter_points(self):
         # Add bonus for a two-day new release rental
-        if self.get_movie().get_price_code() == Movie.NEW_RELEASE and self.get_days_rented() > 1:
-            return 2
-        else:
-            return 1
+        return self._movie.get_frequent_renter_points(self.get_days_rented())
